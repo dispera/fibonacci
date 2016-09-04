2ea8eb6be9b3fc1f9c0dcd3703cbe3b920695bb7
Diego Spera <diego@nexiit.com>
2016-09-04 01:42:43 -0300

    Simplify program

--- fibonacci.rb
+++ fibonacci.rb
@@ -9,20 +9,21 @@ limit = gets.chomp.to_i
 #puts "Not an integer" unless (limit.is_a? Integer)
 
 def fibonacci(max)
-  i = 1
   previous_i = 0
-  total = 1
+  i = total = 1
+
   print "#{i}, "
 
   until i >= max do
-    i += previous_i
-    unless (i + previous_i) >= max
+    new_i = i + previous_i
+    unless (new_i + i) >= max
+      print "#{new_i}, "
       total += 1
-      print "#{i}, "
       previous_i = i
+      i = new_i
     else
+      puts "#{new_i}."
       total += 1
-      puts "#{i}."
       puts "Amount of numbers in this sequence: #{total.to_s}"
       break
     end
